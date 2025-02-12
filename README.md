ce73f551fa294c9470603249a5ea621707f007bb
Brycerw11 <77603372+Brycerw11@users.noreply.github.com>
2025-02-11 18:24:34 -0700

    Finished Project Section layout and started Contact Me sectionon Mobile
    Additionally added a readme.

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Bryce Wahl Personal Portfolio
+
+My personal portfolio website made with HTML, Tailwind CSS, JS, and jQuery.
+
+
+## Authors
+
+- [@Brycerw11](https://github.com/Brycerw11)
+
+
+## Screenshots
+
+![Screenshot of webpage](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## Roadmap
+
+- About Page
+
+- Projects Page
+
+- Contact Page
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ scripts/homepageInteractivity.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    //General Code
+    //if screen width is underneath the "sm" breakpoint in tailwind (if on mobile version of site)
+    if(window.matchMedia("(max-width: 40rem)").matches){
+        //Do things needed for mobile version
+    }
+
+    //Nav Menu Functionality on mobile
+    var isMobileNavMenuOpen = false;
+    $("#hamburger-menu-icon").click(function (){
+        
+        $("#mobile-nav-menu").slideToggle(1000)
+
+        if(isMobileNavMenuOpen){ //if nav menu already open
+            //swap to being closed
+            $(this).css({
+                'transform':'rotate(0deg)'
+            })
+        }
+        else{
+            //swap to being open
+            $(this).css({
+                'transform':'rotate(90deg)'
+            })
+        }
+
+        isMobileNavMenuOpen = !isMobileNavMenuOpen;
+    })
+})
