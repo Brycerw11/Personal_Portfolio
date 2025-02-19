bced4b00580d08c3e3ad77ba753dcb9526c84985
Brycerw11 <77603372+Brycerw11@users.noreply.github.com>
2025-02-18 17:36:34 -0700

    Homepage Misc Project Section Functionality

--- scripts/homepageInteractivity.js
+++ scripts/homepageInteractivity.js
@@ -30,29 +30,98 @@ $(document).ready(function(){
 
     //Miscellaneous Works Section functionality
     var targetProjectIndex = 0;
-    const projectNames = [] //Array of valid projects, by name, in the "projects" object
-    const projects = {
-        "WinFormsFinanceApp": {
+    const projectNames = ["WinForms Finance App", "Java Significant Figure Counter"] //Array of valid projects, by name, in the "projects" object
+    const projects = { //object containing each project as an object
+        "WinForms Finance App": {
             image: "",
-            description: "A personal finance fpplication written using Windows Forms for the .NET Framework in Visual Studio 2022.",
-            gitHubURL: "",
+            description: "A personal finance application written using Windows Forms for the .NET Framework in Visual Studio 2022.",
+            gitHubURL: "https://github.com/Brycerw11/WinFormsFinanceApp",
             tools: ".NET Windows Forms and Visual Studio 2022",
         },
+        "Java Significant Figure Counter": {
+            // image: "./images/project_images/JavaSigFidProgramScreenshot.png",
+            image: "./images/project_images/JavaSigFigProgramScreenshot.png",
+            description: "A small Java program that asks for a number and respondes with the number of significant figures that number has.",
+            gitHubURL: "https://github.com/Brycerw11/java_significant_figure_counter",
+            tools: "Java 17 and IntelliJ IDEA Community Edition 2024",
+        },
+    }
+    
+    //Load Project 1 on page load
+    $("#misc-works-project-header").html(projectNames[0]);
+    $("#misc-project-desc").html(projects[projectNames[0]].description)
+    $("#misc-project-tools").html(projects[projectNames[0]].tools)
+    $("#misc-project-github-link").attr('href', projects[projectNames[0]].gitHubURL)
+
+    function TextAnimation(selector, newText, duration = 1000, step = 1){
+
+        var originalLength = $(selector).html().length
+        
+        //Remove preexisting text
+        var iterations = 0;
+        var removeTextInterval = setInterval(function(){
+            $(selector).html( $(selector).html().substr(0, $(selector).html().length - step) )
+
+            iterations += step;
+            if(iterations >= originalLength){
+                clearInterval(removeTextInterval)
+            }
+
+        }, (duration/2)/ (originalLength / step) )
+
+        setTimeout(function(){ //wait until text is removed then add new text
+            //add new text
+            iterations = 0;
+            var addTextInterval = setInterval(function(){
+                $(selector).html( newText.substr(0, iterations +1) )
+    
+                iterations += step;
+                if(iterations >= newText.length){
+                    clearInterval(addTextInterval)
+                }
+    
+            }, (duration/2)/ (originalLength / step) )
+
+        }, duration/2)
     }
 
     $(".misc-works-button").on('click', function(){
         var direction = $(this).attr("direction");
         
+        //find new targetProjectIndex
         if (direction == "left"){
             targetProjectIndex -= 1;
             if(targetProjectIndex < 0){
                 targetProjectIndex = projectNames.length -1
             }
         }
-        var targetProject = projectNames[targetProjectIndex];
+        else{
+            targetProjectIndex += 1;
+            if(targetProjectIndex >= projectNames.length){
+                targetProjectIndex = 0
+            }
+        }
+
+        //Change to new project
+        var targetProject = projects[projectNames[targetProjectIndex]];
         
-        console.log(targetProject)
+        TextAnimation("#misc-works-project-header", projectNames[targetProjectIndex])
+        TextAnimation("#misc-project-desc", targetProject.description)
+        TextAnimation("#misc-project-tools", targetProject.tools)
+        $("#misc-project-github-link").attr('href', targetProject.gitHubURL)
+
+        var imageUrl = targetProject.image;
+
+        if(targetProject.image == ""){
+            imageUrl = "./images/placeholders/NoImageAvailablePlaceholder.jpg"
+        }
+
+        $("#misc-project-image").animate({opacity: "1%"}, 500) //fade image out
+        setTimeout(function(){ //wait until faded out then switch image and fade in
+            $("#misc-project-image").attr("src", imageUrl).animate({opacity: "100%"}, 500)
+        }, 500) 
     })
+
 
     //To Top Button's Smooth Scroll
     function scrollToTop(){
