68bb895d57babda5347bfae7fa4c72f1f684e340
Brycerw11 <77603372+Brycerw11@users.noreply.github.com>
2025-05-08 18:22:16 -0700

    feat: Set Up Site Structure
    Copy homepage content from my Tailwind_Personal_Portfolio repo and started conversion to sass.

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Bryce Wahl Personal Portfolio
+
+My personal portfolio website made with HTML, Tailwind CSS, JS, and jQuery.
+
+
+## Authors
+
+- [@Brycerw11](https://github.com/Brycerw11)
+
+
+## Screenshots
+
+![Screenshot of Portfolio on Mobile](https://res.cloudinary.com/dtxrpihy6/image/upload/v1739947562/Brycerw11%20Portfolio%20Images/Screenshots/ajnhhvwx9so02omezp7h.png)
+
+![Screenshot of Portfolio on Mobile](https://res.cloudinary.com/dtxrpihy6/image/upload/v1739947562/Brycerw11%20Portfolio%20Images/Screenshots/viwfzupsqbnymwhziczq.png)
+
+
+## Roadmap
+
+- About Page
+
+- Projects Page
+
+- Contact Page
+
+## Wireframes
+- [Homepage Wireframe](https://lucid.app/lucidchart/56c2a135-cb58-4132-859e-d3ec90ce0ae5/edit?invitationId=inv_6985dd5b-1810-4353-9c6b-981c7d9e8c69)
+
+## Acknowledgements
+
+- [Font Awesome v4](https://fontawesome.com/v4)
+- [Tailwind CSS v4.0](https://tailwindcss.com/)
+- [jQuery v3.7.1](https://releases.jquery.com/)
---
+++ scripts/homepageInteractivity.js
@@ -0,0 +1,152 @@
+$(document).ready(function(){
+    //General Code
+    //if the screen width is less than the "sm" breakpoint in tailwind (if on mobile version of site)
+    if(window.matchMedia("(max-width: 40rem)").matches){
+        //Do things needed for mobile version
+    }
+
+    //Nav Menu Functionality on mobile
+    var isMobileNavMenuOpen = false;
+    $("#hamburger-menu-icon").click(function (){
+        
+        $("#mobile-nav-menu").slideToggle(1000)
+
+        if(isMobileNavMenuOpen){ //if nav menu already open
+            //swap to being closed
+            $(this).css({
+                'transform':'rotate(0deg)'
+            })
+        }
+        else{
+            //swap to being open
+            $(this).css({
+                'transform':'rotate(90deg)'
+            })
+        }
+
+        isMobileNavMenuOpen = !isMobileNavMenuOpen;
+    })
+
+
+    //Miscellaneous Works Section functionality
+    var targetProjectIndex = 0;
+    const projectNames = ["WinForms Finance App", "Java Significant Figure Counter"] //Array of valid projects, by name, in the "projects" object
+    const projects = { //object containing each project as an object
+        "WinForms Finance App": {
+            image: "./images/project_images/WinFormsFinanceApp.png",
+            description: "Personal Finance Application written using Windows Forms with the .NET Framework. Developed for the FBLA Coding and Programming Event's 2024-2025 prompt.",
+            gitHubURL: "https://github.com/Brycerw11/WinFormsFinanceApp",
+            tools: ".NET Windows Forms and Visual Studio 2022",
+        },
+        "Java Significant Figure Counter": {
+            image: "./images/project_images/JavaSigFigProgramScreenshot.png",
+            description: "A small Java program that asks for a number and respondes with the number of significant figures that number has.",
+            gitHubURL: "https://github.com/Brycerw11/java_significant_figure_counter",
+            tools: "Java 17 and IntelliJ IDEA Community Edition 2024",
+        },
+    }
+    
+    //Load Project 1 on page load
+    $("#misc-works-project-header").html(projectNames[0]);
+    $("#misc-project-desc").html(projects[projectNames[0]].description)
+    $("#misc-project-tools").html(projects[projectNames[0]].tools)
+    $("#misc-project-github-link").attr('href', projects[projectNames[0]].gitHubURL)
+    $("#misc-project-image").attr("src", projects[projectNames[0]].image)
+
+    function TextAnimation(selector, newText, duration = 1000, step = 1){
+
+        var originalLength = $(selector).html().length
+        
+        //Remove preexisting text
+        var iterations = 0;
+        var removeTextInterval = setInterval(function(){
+            $(selector).html( $(selector).html().substr(0, $(selector).html().length - step) )
+
+            iterations += step;
+            if(iterations >= originalLength){
+                clearInterval(removeTextInterval)
+            }
+
+        }, (duration/2)/ (originalLength / step) )
+
+        setTimeout(function(){ //wait until text is removed then add new text
+            //add new text
+            iterations = 0;
+            var addTextInterval = setInterval(function(){
+                $(selector).html( newText.substr(0, iterations +1) )
+    
+                iterations += step;
+                if(iterations >= newText.length){
+                    clearInterval(addTextInterval)
+                }
+    
+            }, (duration/2)/ (originalLength / step) )
+
+        }, duration/2)
+
+        setTimeout(function(){ //ensure the new text is fully displayed at the end of transition duration
+            $(selector).html( newText )
+        }, duration+100)
+    }
+
+    // Change misc work when one of the buttons is clicked
+    $(".misc-works-button").on('click', function(){
+        var direction = $(this).attr("direction");
+        
+        //find new targetProjectIndex
+        if (direction == "left"){
+            targetProjectIndex -= 1;
+            if(targetProjectIndex < 0){
+                targetProjectIndex = projectNames.length -1
+            }
+        }
+        else{
+            targetProjectIndex += 1;
+            if(targetProjectIndex >= projectNames.length){
+                targetProjectIndex = 0
+            }
+        }
+
+        //Change to new project
+        var targetProject = projects[projectNames[targetProjectIndex]];
+        
+        // Change Text
+        TextAnimation("#misc-works-project-header", projectNames[targetProjectIndex])
+        TextAnimation("#misc-project-desc", targetProject.description)
+        TextAnimation("#misc-project-tools", targetProject.tools)
+        // Change Links
+        $("#misc-project-github-link").attr('href', targetProject.gitHubURL)
+
+        // Find new url for the image
+        var imageUrl = targetProject.image;
+
+        if(targetProject.image == ""){ //replace image with a placeholder if an image is not specified
+            imageUrl = "./images/placeholders/NoImageAvailablePlaceholder.jpg"
+        }
+
+        // Change image
+        $("#misc-project-image").animate({opacity: "1%"}, 500) //fade image out
+        setTimeout(function(){ //wait until faded out then switch image and fade in
+            $("#misc-project-image").attr("src", imageUrl).animate({opacity: "100%"}, 500)
+        }, 500) 
+    })
+
+
+    //To Top Button's Smooth Scroll
+    function scrollToTop(){
+        //uses an animation to scroll to the top over 700 miliseconds instead of instantly
+        $('html, body').animate({ 
+            scrollTop: 0
+        }, 700);
+    }
+
+    $("#compact-to-top-button").on('click', function( event){   
+        event.preventDefault(); //overrides default scroll to top functionality
+        scrollToTop();
+    })
+    $("#expanded-to-top-button").on('click', function( event){   
+        event.preventDefault(); //overrides default scroll to top functionality
+        scrollToTop();
+    })
+
+})
